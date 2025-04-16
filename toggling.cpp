19bf6fdb7713660150bfe9811f0539b244e4ed8d
giri_mallik <94897812+sunnylg-jc@users.noreply.github.com>
2025-04-15 23:56:42 -0700

    toggling.cpp
    updated

--- toggling.cpp
+++ toggling.cpp
@@ -1,14 +1,24 @@
-#include <iostream>
-#include <bitset>
+#include<iostream>
+#include<bitset>
+int main()
+{
+    int num = 0b00110101;  //the bit that we are gonna perform toggling
+    int bit_to_toggle;
+    std::cout << "Enter the bit you want to toggle" << std::endl;
+    std::cin >> bit_to_toggle;
 
-int main() {
-    int num = 0b00001100; // 12 in decimal
-    std::cout << "Original: " << std::bitset<8>(num) << std::endl;
+    //creating a masked bit
+    int masked_bit = (1 << bit_to_toggle);
 
-    int bit_to_toggle = 3;
-    num = num ^ (1 << bit_to_toggle); // Toggle the 3rd bit
-
-    std::cout << "Toggled : " << std::bitset<8>(num) << std::endl;
-
+    //toggling a bit
+    int toggled_bit;
+    toggled_bit = num ^ masked_bit;
+    
+    std::cout << "original bit is  "<< std::bitset<8>(num) << std::endl;
+    std::cout << "masked bit is    "<< std::bitset<8>(masked_bit) << std::endl;
+    std::cout << "               ^"  << std::endl;
+    std::cout << "                 --------"  << std::endl;
+    std::cout << "toggled bit is   "<< std::bitset<8>(toggled_bit) << std::endl;
+    std::cout << "                 --------"  << std::endl;
     return 0;
 }
